ff6321ca51dbe4c324904f2f28baddf0746d6a91
Seungmin Yi <s2019w33@e-mirim.hs.kr>
2020-08-16 13:55:46 +0900

    텍스트 검증 예제

---
+++ android/app/src/main/kotlin/kr/hs/emirim/julianneyi/flutter_app_edit2/MainActivity.kt
@@ -0,0 +1,6 @@
+package kr.hs.emirim.julianneyi.flutter_app_edit2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '폼 검증 데모',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('폼 검증 데모'),
+        ),
+        body: MyCustomForm(),
+      ),
+    );
+  }
+}
+
+class MyCustomForm extends StatefulWidget {
+  @override
+  _MyCustomFormState createState() => _MyCustomFormState();
+}
+
+class _MyCustomFormState extends State<MyCustomForm> {
+  final _formkey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formkey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            validator: (value) {
+              if(value.isEmpty) {
+                return '글자를 입력하세요';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: RaisedButton(
+              onPressed: () {
+                if (_formkey.currentState.validate()) {
+                  Scaffold.of(context)
+                      .showSnackBar(SnackBar(content: Text('검증 완료'),));
+                }
+              },
+              child: Text('검증'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
